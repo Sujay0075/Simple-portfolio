ed46c34a58abc76fd9d08b07b4aa8b92ad481759
Sujay0075 <154050729+Sujay0075@users.noreply.github.com>
2025-05-16 16:38:05 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,6 +0,0 @@
-const sujay = document.querySelector(".sujay");
-const menu = document.querySelector(".navbar-menu .menu-bar");
-
-sujay.addEventListener("click",()=> {
-    menu.classList.toggle("show")
-})
